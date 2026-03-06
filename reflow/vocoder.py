946906c42d9eaa1b055f7b6a694e5289ca474cea
Eric Wang <43533746+Pitiedwzr@users.noreply.github.com>
2026-03-07 10:04:43 +1300

    fix: wrong index of speaker embedding
    Adjust speaker embedding calculations to use 0-based indexing.

--- reflow/vocoder.py
+++ reflow/vocoder.py
@@ -222,9 +222,11 @@ class Unit2Wav(nn.Module):
             true_spk_emb = torch.zeros((units.shape[0], 256), device=units.device)
             for k, v in spk_mix_dict.items():
                 mix_id = torch.LongTensor(np.array([[int(k)]])).to(units.device)
-                true_spk_emb += self.spk_embed(mix_id.squeeze(-1)) * v
+                # Subtract 1 here to map 1-based ID to 0-based PyTorch index
+                spk_emb += self.spk_embed(mix_id.squeeze(-1) - 1) * v
         elif spk_id is not None:
-            true_spk_emb = self.spk_embed(spk_id.squeeze(-1)) 
+            # Subtract 1 here as well
+            spk_emb = self.spk_embed(spk_id.squeeze(-1) - 1) 
         else:
             true_spk_emb = torch.zeros((units.shape[0], 256), device=units.device)
             
